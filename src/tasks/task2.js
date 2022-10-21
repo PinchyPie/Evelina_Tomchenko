8d99265ea6f146a82ba9f9d70c2210b8f626860e
pinchypie <pinchypie03@gmail.com>
2022-10-21 13:13:22 +0300

    first_non_repeated_letter

---
+++ src/tasks/task2.js
@@ -0,0 +1,15 @@
+export const func2 = () => {
+    function first_non_repeated_letter(exp){
+        for(let item of exp){
+          let count = 0;
+          let index = exp.indexOf(item)
+          while(index != -1){
+              count++ 
+              index = exp.indexOf(item, index + 1)
+          }
+          if(count === 1){
+              return item
+          }
+        }
+      }
+}
