ca8d4290d359722afd441a6a9617dfdd7051fe39
Evelina <79174840+PinchyPie@users.noreply.github.com>
2023-01-10 22:56:12 +0200

    Add files via upload

---
+++ spec/test-constants.ts
@@ -0,0 +1,8 @@
+// add valid token before running tests
+export const accessToken =
+  "sl.BWNpz7qeG7T1q7tsvKbBvwjukvc5A7n-HahgO9e9o_LsxWC0_beiUAYdtUf0Vkx25SEEBKdhgS8MGPlLXfQxS20vcjaMqaLs0T99ve_qOpN6GFHFAfR-R0gGQBdc1CkL_TwYus0";
+export const expiredAccessToken =
+  "sl.BWPV_nLriGIUiB0I3IeDAXvwA3SzbYnr9eOYM_tHIK5_YMa3rQmLFpkQdyKWVzJFy_vEigZPT1MMH88Dglyd7ZT4ykDaIwUV3jF5g0u0oEYZCwA6Ro1njSzEVY920X8EmkuigE0";
+export const dropboxPath = "/test.txt";
+export const incorrectDropboxPath = "/incorrect-test.txt";
+export const fileName = "test.txt";
